fdb862981cfdaa2f0be5c3990647c9002ef2a860
Victor Chacon <victorchacon@Victors-Air.fios-router.home>
2017-08-17 01:19:51 -0700

    Material design test application

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,39 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MdButtonModule,
+  MdCardModule,
+  MdMenuModule,
+  MdToolbarModule,
+  MdIconModule,
+  MdDatepickerModule,
+  MdNativeDateModule,
+  MdInputModule } from '@angular/material';
+import {CdkTableModule} from '@angular/cdk';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    FormsModule,
+    ReactiveFormsModule,
+    HttpModule,
+    MdButtonModule,
+    MdCardModule,
+    MdMenuModule,
+    MdToolbarModule,
+    MdIconModule,
+    MdDatepickerModule,
+    MdNativeDateModule,
+    MdInputModule
   ],
   providers: [],
   bootstrap: [AppComponent]
